0d06e54ea5d96c3a9043bc5b6930b7fb2aaa63c3
bxsakura95 <113070809+bxsakura95@users.noreply.github.com>
2023-01-19 20:03:22 -0700

    Add files via upload

---
+++ Homework1/sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+		createCanvas(400,400);
+}
+
+function draw() {
+	background(233,205,208);
+	fill(229,211,179)
+	rect(60,290,50,110);
+	rect(290,290,50,110);
+	rect(60,210,280,140);
+	fill(232,255,245)
+	rect(80,230,240,100);
+	fill(255,255,255)
+	circle(310,250,15);
+	circle(290,280,15);
+	circle(310,310,15);
+	circle(100,310,15);
+	circle(110,280,15);
+	circle(90,250,15);
+	fill(172,216,230)
+	circle(200,250,190);
+	fill(252,214,127)
+	ellipse(200,260,160,120);
+	ellipse(200,240,160,120);
+	ellipse(200,220,160,120);
+	ellipse(200,200,160,120);
+	fill(181,101,29)
+	ellipse(200,200,130,90);
+	fill(253,239,178)
+	rect(175,175,50,40);
+	fill(100,0,100)
+	circle(180,270,10);
+	circle(260,290,10);
+	circle(250,238,10);
+	circle(220,290,10);
+	circle(180,310,10);
+	circle(155,300,10);
+	circle(140,270,10);
+	circle(150,238,10);
+	circle(260,270,10);
+	circle(260,170,10);
+	circle(140,170,10);
+	circle(220,150,10);
+	textSize(26)
+	text("Huckleberry Pancakes",60,30);
+	textSize(16)
+	text("By Brenna Bibler",130,60);
+	
+	
+	
+	
+	
+}
